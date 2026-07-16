404aaa16c91dbd740fc57598830c887add14ea79
malkilina04-hue <malkilina04@gmail.com>
2026-07-16 13:31:56 +0200

    Ajout des pages et configuration React Router

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+    <main>
+        <div className="hero">
+        <section className="hero-content">
+            <p className="subtitle">No fees.</p>
+            <p className="subtitle">No minimum deposit.</p>
+            <p className="subtitle">High interest rates.</p>
+            <p className="text">Open a savings account with Argent Bank today!</p>
+        </section>
+        </div>
+    </main>
+    )
+}
+
+export default Home
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+    <main>
+        <h1>Login Page</h1>
+    </main>
+    )
+}
+
+export default Login
